14f906a6719f2fd42c5a779d1101ef4c62e84488
Anselmo <anselmo.mda@outlook.com>
2025-03-07 14:46:28 -0300

    Melhora na execução por causa do HTTPS , o certificado está causando problema na execução. Retorno da API invocadora e API de regras alterados também

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,46 +18,57 @@ namespace BRD_NF_4_7_2_TRANSMISSAO.Controllers
         [HttpPost]
         public async Task<ActionResult> Index(HttpPostedFileBase arquivo, string descricao)
         {
-            if (arquivo == null || string.IsNullOrEmpty(descricao))
+            try
             {
-                ViewBag.Mensagem = "Por favor, selecione um arquivo e forneça o tipo de arquivo.";
-                return View();
-            }
-
-            using (var client = new HttpClient())
-            {
-                string baseUrl = ConfigurationManager.AppSettings["BaseUrlApi"];
-                string endPoint = ConfigurationManager.AppSettings["EndPoint"];
-
-                client.BaseAddress = new Uri(baseUrl); // Troque pela URL base da sua API
-
-                using (var content = new MultipartFormDataContent())
+                if (arquivo == null || string.IsNullOrEmpty(descricao))
                 {
-                    var fileContent = new StreamContent(arquivo.InputStream);
-                    fileContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data")
-                    {
-                        Name = "\"arquivo\"",
-                        FileName = "\"" + arquivo.FileName + "\""
-                    };
-                    fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(arquivo.ContentType);
-                    content.Add(fileContent);
-                    content.Add(new StringContent(descricao), "\"tipoArquivo\"");
+                    ViewBag.Mensagem = "Por favor, selecione um arquivo e forneça o tipo de arquivo.";
+                    return View();
+                }
 
-                    var response = await client.PostAsync(endPoint, content);
+                var handler = new HttpClientHandler();
+                handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
 
-                    if (response.IsSuccessStatusCode)
+                using (var client = new HttpClient(handler))
+                {
+                    string baseUrl = ConfigurationManager.AppSettings["BaseUrlApi"];
+                    string endPoint = ConfigurationManager.AppSettings["EndPoint"];
+
+                    client.BaseAddress = new Uri(baseUrl); // Troque pela URL base da sua API
+
+                    using (var content = new MultipartFormDataContent())
                     {
-                        ViewBag.Mensagem = "Arquivo processado corretamente!";
-                    }
-                    else
-                    {
-                        var responseContent = await response.Content.ReadAsStringAsync();
-                        ViewBag.Mensagem = responseContent;
+                        var fileContent = new StreamContent(arquivo.InputStream);
+                        fileContent.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("form-data")
+                        {
+                            Name = "\"arquivo\"",
+                            FileName = "\"" + arquivo.FileName + "\""
+                        };
+                        fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(arquivo.ContentType);
+                        content.Add(fileContent);
+                        content.Add(new StringContent(descricao), "\"tipoArquivo\"");
+
+                        var response = await client.PostAsync(endPoint, content);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            ViewBag.Mensagem = "Arquivo processado corretamente!";
+                        }
+                        else
+                        {
+                            var responseContent = await response.Content.ReadAsStringAsync();
+                            ViewBag.Mensagem = responseContent;
+                        }
                     }
                 }
-            }
 
-            return View();
+                return View();
+            }
+            catch (Exception ex)
+            {
+                //return "Erro API Regras : " + ex.InnerException;
+                return Content("Ocorreu um erro no frontend Bradesco: " + ex.Message);
+            }
         }
     }
 }
